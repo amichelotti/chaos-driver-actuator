eb767a60bdcebc6b0537a82dc4772252cfcbd230
aduffizi <alessandro.duffizi@lnf.infn.it>
2017-01-31 11:32:06 +0100

    bool mode in hardreset

--- core/ChaosActuatorInterface.cpp
+++ core/ChaosActuatorInterface.cpp
@@ -310,7 +310,7 @@ int ChaosActuatorInterface::moveAbsoluteMillimeters(int32_t axisID,double mm) {
 int ChaosActuatorInterface::stopMotion(int32_t axisID){
     WRITE_OP_AX_TIM(OP_STOP_MOTION,axisID,0);
 }
-int ChaosActuatorInterface::hardreset(int32_t axisID,int32_t mode){
+int ChaosActuatorInterface::hardreset(int32_t axisID,bool mode){
     WRITE_OP_AX_INT_TIM(OP_HARD_RESET,axisID,mode,0);
 }
 int ChaosActuatorInterface::homing(int32_t axisID,homingType mode){
