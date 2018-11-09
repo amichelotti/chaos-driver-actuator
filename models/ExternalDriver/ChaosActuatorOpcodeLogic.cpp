1f3c5d3cc7d4bd0ef188cb686b52bd6213c87a09
aduffizi <alessandro.duffizi@lnf.infn.it>
2018-11-09 17:52:57 +0100

    messe doppie parentesi per silenziare warning

--- models/ExternalDriver/ChaosActuatorOpcodeLogic.cpp
+++ models/ExternalDriver/ChaosActuatorOpcodeLogic.cpp
@@ -83,7 +83,7 @@ WRITE_ERR_ON_CMD(c, -2, "'err' key not found on external driver return package",
 #define SEND_REQUEST_OPC(opc,c, r,a) {\
     int err;\
 try { \
-if(err=sendOpcodeRequest(opc,ChaosMoveOperator(r),a)) {\
+if((err=sendOpcodeRequest(opc,ChaosMoveOperator(r),a))) {\
 WRITE_ERR_ON_CMD(err, -1, "Error from from remote driver", __PRETTY_FUNCTION__);\
 }else {\
     if(response->hasKey("err")) {\
