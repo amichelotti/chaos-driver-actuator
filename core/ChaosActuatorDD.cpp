4c1014546fddd71a9372d3df798e91a947711804
amichelo <andrea.michelotti@lnf.infn.it>
2020-11-21 00:25:21 +0100

    sanitized

--- core/ChaosActuatorDD.cpp
+++ core/ChaosActuatorDD.cpp
@@ -1,5 +1,5 @@
 /*
- *	Actuator base 
+ *	Actuator base
  *	!CHAOS
  *	Created by Alessandro D'Uffizi
  *
@@ -19,174 +19,174 @@
  */
 #include "ChaosActuatorDD.h"
 
-#include <string>
+#include "driver/actuator/core/ChaosActuatorInterface.h"
 #include <boost/regex.hpp>
 #include <chaos/cu_toolkit/driver_manager/driver/AbstractDriverPlugin.h>
-#include "driver/actuator/core/ChaosActuatorInterface.h"
+#include <string>
 
-
-#define ACLAPP		LAPP_ << "[ActuatorDD] "
-#define ACDBG		LDBG_ << "[ActuatorDD] "
-#define ACERR		LERR_ << "[ActuatorDD] "
+#define ACLAPP LAPP_ << "[ActuatorDD] "
+#define ACDBG LDBG_ << "[ActuatorDD] "
+#define ACERR LERR_ << "[ActuatorDD] "
 
 using namespace chaos::driver::actuator;
-//default constructor definition
-DEFAULT_CU_DRIVER_PLUGIN_CONSTRUCTOR_WITH_NS(chaos::driver::actuator, ChaosActuatorDD) {
-    motor = NULL;
-	
+// default constructor definition
+DEFAULT_CU_DRIVER_PLUGIN_CONSTRUCTOR_WITH_NS(chaos::driver::actuator,
+                                             ChaosActuatorDD) {
+  motor = NULL;
 }
 
-//default destructor
-ChaosActuatorDD::~ChaosActuatorDD() {
-	
+// default destructor
+ChaosActuatorDD::~ChaosActuatorDD() {}
+
+void ChaosActuatorDD::driverDeinit() {
+  if (motor) {
+    delete motor;
+  }
+  motor = NULL;
 }
 
-void ChaosActuatorDD::driverDeinit()  {
-    if(motor){
-        delete motor;
-    }
-    motor = NULL;
-}
+cu_driver::MsgManagmentResultType::MsgManagmentResult
+ChaosActuatorDD::execOpcode(cu_driver::DrvMsgPtr cmd) {
+ // boost::mutex::scoped_lock lock(io_mux);
 
-
-cu_driver::MsgManagmentResultType::MsgManagmentResult ChaosActuatorDD::execOpcode(cu_driver::DrvMsgPtr cmd){
-        boost::mutex::scoped_lock lock(io_mux);
-
-    cu_driver::MsgManagmentResultType::MsgManagmentResult result = cu_driver::MsgManagmentResultType::MMR_EXECUTED;
-    actuator_iparams_t *in = (actuator_iparams_t *)cmd->inputData;
-    actuator_oparams_t *out = (actuator_oparams_t *)cmd->resultData;
-    if(motor==NULL){
-      ACERR<<"motor low level driver NULL, executing opcode:"<<cmd->opcode;
-      return result;
-    }
-    switch(cmd->opcode){
-        case OP_INIT:
-             ACDBG<< "Initializing";
-             out->result = motor->init(in->str);
-            break;
-            
-        case OP_CONFIGAXIS:
-             ACDBG<< "Configuring " << in->str;
-             out->result = motor->configAxis(in->str);
-            break;
-        case OP_DEINIT:
-             ACDBG<< "Deinitializing";
-             out->result = motor->deinit(in->axis);
-            break;
-          
-        case OP_GET_POSITION:
-            out->result=motor->getPosition(in->axis,(::common::actuators::AbstractActuator::readingTypes)in->ivalue,&out->fvalue0);
-          //  ACDBG<<"Got Position :"<< out->fvalue0;
-            break; 
-            
-        case OP_RESET_ALARMS:
-            ACDBG<<"Reset alarms to:"<<in->alarm_mask << std::endl;
-            out->result = motor->resetAlarms(in->axis,in->alarm_mask);
-            break;
-        case OP_HARD_RESET:
-            ACDBG<<"HARD Reset in axis "<< in->axis << std::endl;
-            out->result = motor->hardreset(in->axis,in->ivalue);
-            break;
-        case OP_GET_ALARMS: {
-	    std::string desc;
-            out->result = motor->getAlarms(in->axis,&out->alarm_mask,desc);
-            strncpy(out->str,desc.c_str(),MAX_STR_SIZE);
-        //    ACDBG<<"Got alarms to: "<<out->alarm_mask << desc;
-            break;
-           }
-            
-         case OP_MOVE_RELATIVE_MM:
-            out->result = motor->moveRelative(in->axis,in->fvalue0);
-            ACDBG<<"Move relative result:"<<out->result;
-            break;
-        
-        case OP_MOVE_ABSOLUTE_MM:
-            out->result = motor->moveAbsolute(in->axis,in->fvalue0);
-            ACDBG<<"Moved Absolute result:"<<out->result;
-            break;
-       
-        case OP_STOP_MOTION:
-            ACDBG<<"Before Stop Motion :" << std::endl;
-            out->result = motor->stopMotion(in->axis);
-            ACDBG<<"Stop Motion, result:"<< out->result;
-            break;
-        
-        case OP_HOMING:
-            out->result = motor->homing(in->axis, (::common::actuators::AbstractActuator::homingType)in->ivalue );
-            ACDBG<<"Set homing, homing type: "<< in->ivalue << "result is " << out->result;
-            break;
-            
-            
-        case OP_POWERON:
-            out->result = motor->poweron(in->axis,in->ivalue);
-            ACDBG<<"Set Power" << in->ivalue <<" , result:"<< out->result;
-            break;
-            
-         case OP_GET_STATE:{
-            std::string desc;
-            out->result = motor->getState(in->axis,&out->ivalue,desc);
-            strncpy(out->str,desc.c_str(),MAX_STR_SIZE);
-        //    ACDBG<<"Got State: "<<out->ivalue<<" \""<<desc;
-            break;
-        }
-            
-        case OP_GET_SWVERSION:{
-            std::string ver;
-            out->result = motor->getSWVersion(in->axis,ver);
-            ACDBG<<"Got HW Version:\""<<ver;
-            strncpy(out->str,ver.c_str(),MAX_STR_SIZE);;
-
-            break;
-        }
-        case OP_GET_HWVERSION:{
-            std::string ver;
-            out->result = motor->getHWVersion(in->axis,ver);
-            ACDBG<<"Got SW Version:\""<<ver;
-            strncpy(out->str,ver.c_str(),MAX_STR_SIZE);;
-
-            break;
-        }            
-        case OP_LISTPARAMETERS:{
-        std::string dataset;
-	out->result=motor->listParameters(dataset);
-	//ACDBG << "ALEDEBUG: Received dataset " <<  dataset;
-        strncpy(out->str,dataset.c_str(),JSON_MAX_SIZE);
-	break;
-	}
-        case OP_SETPARAMETER:{
-            char* SS0=strdup(in->str);
-	char* SS1=strdup(in->str2);
-	out->result=motor->setParameter(in->axis,SS0,SS1);
-	
-	ACDBG << "ALEDEBUG: Sending Set " << SS1 <<"  on Parameter " << SS0 <<"axis " <<in->axis;
-	ACDBG << "result " << out->result ;
-	free(SS0);
-	free(SS1);
-	break;
-	}
-        case OP_GETPARAMETER: {
-        	std::string tempString;
-        	out->result=motor->getParameter(in->axis,in->str,tempString);
-
-        	strncpy(out->str,tempString.c_str(),JSON_MAX_SIZE);
-        	ACDBG << "ALEDEBUG (DD) asked for " << in->str << " received " <<out->str;
-        	break;
-        }
-            
-        case OP_GET_FEATURE:{
-            uint64_t feat=motor->getFeatures();
-            out->alarm_mask=feat;
-       //     ACDBG<<"Got Features:"<<feat;
-        }
-            break;
-        
-        default:
-            ACERR<<"Opcode not supported:"<<cmd->opcode;
-    }
+  cu_driver::MsgManagmentResultType::MsgManagmentResult result =
+      cu_driver::MsgManagmentResultType::MMR_EXECUTED;
+  actuator_iparams_t *in = (actuator_iparams_t *)cmd->inputData;
+  actuator_oparams_t *out = (actuator_oparams_t *)cmd->resultData;
+  if (motor == NULL) {
+    ACERR << "motor low level driver NULL, executing opcode:" << cmd->opcode;
     return result;
+  }
+  switch (cmd->opcode) {
+  case OP_INIT:
+    ACDBG << "Initializing";
+    out->result = motor->init(in->str);
+    break;
+
+  case OP_CONFIGAXIS:
+    ACDBG << "Configuring " << in->str;
+    out->result = motor->configAxis(in->str);
+    break;
+  case OP_DEINIT:
+    ACDBG << "Deinitializing";
+    out->result = motor->deinit(in->axis);
+    break;
+
+  case OP_GET_POSITION: {
+
+    out->result = motor->getPosition(
+        in->axis,
+        (::common::actuators::AbstractActuator::readingTypes)in->ivalue,
+        &out->fvalue0);
+    //  ACDBG<<"Got Position :"<< out->fvalue0;
+    break;
+  }
+
+  case OP_RESET_ALARMS:
+    ACDBG << "Reset alarms to:" << in->alarm_mask << std::endl;
+    out->result = motor->resetAlarms(in->axis, in->alarm_mask);
+    break;
+  case OP_HARD_RESET:
+    ACDBG << "HARD Reset in axis " << in->axis << std::endl;
+    out->result = motor->hardreset(in->axis, in->ivalue);
+    break;
+  case OP_GET_ALARMS: {
+    std::string desc;
+    out->result = motor->getAlarms(in->axis, &out->alarm_mask, desc);
+    strncpy(out->str, desc.c_str(), MAX_STR_SIZE);
+    //    ACDBG<<"Got alarms to: "<<out->alarm_mask << desc;
+    break;
+  }
+
+  case OP_MOVE_RELATIVE_MM:
+    out->result = motor->moveRelative(in->axis, in->fvalue0);
+    ACDBG << "Move relative result:" << out->result;
+    break;
+
+  case OP_MOVE_ABSOLUTE_MM:
+    out->result = motor->moveAbsolute(in->axis, in->fvalue0);
+    ACDBG << "Moved Absolute result:" << out->result;
+    break;
+
+  case OP_STOP_MOTION:
+    ACDBG << "Before Stop Motion :" << std::endl;
+    out->result = motor->stopMotion(in->axis);
+    ACDBG << "Stop Motion, result:" << out->result;
+    break;
+
+  case OP_HOMING:
+    out->result = motor->homing(
+        in->axis,
+        (::common::actuators::AbstractActuator::homingType)in->ivalue);
+    ACDBG << "Set homing, homing type: " << in->ivalue << "result is "
+          << out->result;
+    break;
+
+  case OP_POWERON:
+    out->result = motor->poweron(in->axis, in->ivalue);
+    ACDBG << "Set Power" << in->ivalue << " , result:" << out->result;
+    break;
+
+  case OP_GET_STATE: {
+    std::string desc;
+    out->result = motor->getState(in->axis, &out->ivalue, desc);
+    strncpy(out->str, desc.c_str(), MAX_STR_SIZE);
+    //    ACDBG<<"Got State: "<<out->ivalue<<" \""<<desc;
+    break;
+  }
+
+  case OP_GET_SWVERSION: {
+    std::string ver;
+    out->result = motor->getSWVersion(in->axis, ver);
+    ACDBG << "Got HW Version:\"" << ver;
+    strncpy(out->str, ver.c_str(), MAX_STR_SIZE);
+    ;
+
+    break;
+  }
+  case OP_GET_HWVERSION: {
+    std::string ver;
+    out->result = motor->getHWVersion(in->axis, ver);
+    ACDBG << "Got SW Version:\"" << ver;
+    strncpy(out->str, ver.c_str(), MAX_STR_SIZE);
+    ;
+
+    break;
+  }
+  case OP_LISTPARAMETERS: {
+    std::string dataset;
+    out->result = motor->listParameters(dataset);
+    // ACDBG << "ALEDEBUG: Received dataset " <<  dataset;
+    strncpy(out->str, dataset.c_str(), JSON_MAX_SIZE);
+    break;
+  }
+  case OP_SETPARAMETER: {
+    std::string SS0(in->str);
+    std::string SS1(in->str2);
+    out->result = motor->setParameter(in->axis, SS0, SS1);
+
+    /*ACDBG << "ALEDEBUG: Sending Set " << SS1 << "  on Parameter " << SS0
+          << "axis " << in->axis;
+    ACDBG << "result " << out->result;*/
+    break;
+  }
+  case OP_GETPARAMETER: {
+    std::string tempString;
+    out->result = motor->getParameter(in->axis, in->str, tempString);
+
+    strncpy(out->str, tempString.c_str(), JSON_MAX_SIZE);
+    ACDBG << "ALEDEBUG (DD) asked for " << in->str << " received " << out->str;
+    break;
+  }
+
+  case OP_GET_FEATURE: {
+    uint64_t feat = motor->getFeatures();
+    out->alarm_mask = feat;
+    //     ACDBG<<"Got Features:"<<feat;
+  } break;
+
+  default:
+    ACERR << "Opcode not supported:" << cmd->opcode;
+  }
+  return result;
 }
-
-
-
-
-
--- core/ChaosActuatorInterface.cpp
+++ core/ChaosActuatorInterface.cpp
@@ -12,285 +12,287 @@ using namespace chaos::driver::actuator;
 #define SCCUERR ERR_LOG(ChaosActuatorInterface) << "[" << owner << "]- "<<accessor->getMessageCount()<<"-"
 
 
+#define RETURN \
+    {int tmp=ret->result;free(message.inputData);free(message.resultData);return tmp;}
+
+#define SEND_AND_RETURN \
+ accessor->send(&message);			\
+RETURN
+
+#define SEND_AND_RETURN_TIM(t) \
+ accessor->send(&message,t);			\
+RETURN
+
+
+
 #define PREPARE_OP_RET_INT_TIMEOUT(op,tim) \
-actuator_oparams_t ret;\
-actuator_iparams_t idata;\
-memset(&ret,0,sizeof(ret));memset(&idata,0,sizeof(idata));\
-ret.result=DRV_BYPASS_DEFAULT_CODE;\
-strncpy(idata.cuname,owner.c_str(),MAX_STR_SIZE);\
+actuator_oparams_t* ret=(actuator_oparams_t*)calloc(1,sizeof(actuator_oparams_t));\
+actuator_iparams_t* idata=(actuator_iparams_t*)calloc(1,sizeof(actuator_iparams_t));\
+ret->result=DRV_BYPASS_DEFAULT_CODE;\
+idata->timeout=tim;\
 message.opcode = op; \
-message.inputData=(void*)&idata;\
-idata.timeout=tim;\
+message.inputData=(void*)idata;\
 message.inputDataLength=sizeof(actuator_iparams_t);\
 message.resultDataLength=sizeof(actuator_oparams_t);\
-message.resultData = (void*)&ret;\
+message.resultData = (void*)ret;\
 
 /***************************/
 #define READ_OP_FLOAT_PARAM_INT(op,ival,pfval,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-idata.ivalue=ival; \
-ret.fvalue0=*pfval;\
+idata->ivalue=ival; \
+ret->fvalue0=*pfval;\
 accessor->send(&message); \
-*pfval = ret.fvalue0; \
-return ret.result; 
+*pfval = ret->fvalue0; \
+RETURN
 
 #define READ_OP_AX_FLOAT_PARAM_INT(op,ax,ival,pfval,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-idata.axis=ax;\
-idata.ivalue=ival; \
-ret.fvalue0=*pfval;\
+idata->axis=ax;\
+idata->ivalue=ival; \
+ret->fvalue0=*pfval;\
    accessor->send(&message); \
-*pfval = ret.fvalue0; \
-return ret.result; 
+*pfval = ret->fvalue0; \
+RETURN
+
 /***************************/
 
 #define WRITE_OP_INT_TIM_NORET(op,ival,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-idata.ivalue=ival; \
- accessor->send(&message);	
- 
+idata->ivalue=ival; \
+ accessor->send(&message);\
+free(message.inputData);free(message.resultData); 
+
 #define WRITE_OP_AX_INT_TIM_NORET(op,ax,ival,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-idata.axis=ax;\
-idata.ivalue=ival; \
- accessor->send(&message);	
-
+idata->axis=ax;\
+idata->ivalue=ival; \
+ accessor->send(&message);	\
+free(message.inputData);free(message.resultData);
 
 /***************************/
 #define WRITE_OP_AX_INT_TIM(op,ax,ival,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-idata.axis=ax;\
-idata.ivalue=ival; \
-ret.result=0;\
- accessor->send(&message);	\
-return ret.result;
+idata->axis=ax;\
+idata->ivalue=ival; \
+ret->result=0;\
+SEND_AND_RETURN
 
 #define WRITE_OP_INT_TIM(op,ival,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-idata.ivalue=ival; \
-ret.result=0;\
- accessor->send(&message);	\
-return ret.result;
+idata->ivalue=ival; \
+ret->result=0;\
+SEND_AND_RETURN
+
 /***************************/
 #define WRITE_OP_64INT_TIM(op,ival,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-idata.alarm_mask=ival;\
-ret.result=0;\
-accessor->send(&message);\
-return ret.result;
+idata->alarm_mask=ival;\
+ret->result=0;\
+SEND_AND_RETURN
 
 #define WRITE_OP_AX_64INT_TIM(op,ax,ival,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-idata.axis=ax;\
-idata.alarm_mask=ival;\
-ret.result=0;\
-accessor->send(&message);\
-return ret.result;
+idata->axis=ax;\
+idata->alarm_mask=ival;\
+ret->result=0;\
+SEND_AND_RETURN
 
 /***************************/
 #define WRITE_OP_AX_FLOAT_TIM(op,ax,fval,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-idata.axis=ax;\
-idata.fvalue0=fval;\
-ret.result=0;\
-accessor->send(&message);	\
-return ret.result;
+idata->axis=ax;\
+idata->fvalue0=fval;\
+ret->result=0;\
+SEND_AND_RETURN
 
 #define WRITE_OP_FLOAT_TIM(op,fval,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-idata.fvalue0=fval;\
-ret.result=0;\
-accessor->send(&message);	\
-return ret.result;
+idata->fvalue0=fval;\
+ret->result=0;\
+SEND_AND_RETURN
+
 /***************************/
 #define WRITE_OP_AX_2FLOAT_TIM(op,ax,fval0,fval1,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-idata.axis=ax;\
-idata.fvalue0=fval0;\
-idata.fvalue1=fval1;\
-ret.result=0;\
-accessor->send(&message);\
-return ret.result;
-
+idata->axis=ax;\
+idata->fvalue0=fval0;\
+idata->fvalue1=fval1;\
+ret->result=0;\
+SEND_AND_RETURN
 
 #define WRITE_OP_2FLOAT_TIM(op,fval0,fval1,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-idata.fvalue0=fval0;\
-idata.fvalue1=fval1;\
-ret.result=0;\
-accessor->send(&message);\
-return ret.result;
+idata->fvalue0=fval0;\
+idata->fvalue1=fval1;\
+ret->result=0;\
+SEND_AND_RETURN
+
 /***************************/
 #define READ_OP_AX_FLOAT_TIM(op,ax,pfval,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-idata.axis=ax;\
-ret.fvalue0=*pfval;\
-ret.result=0;\
+idata->axis=ax;\
+ret->fvalue0=*pfval;\
+ret->result=0;\
 accessor->send(&message);\
-*pfval = ret.fvalue0;\
-return ret.result;
-
+*pfval = ret->fvalue0;\
+RETURN
 
 #define READ_OP_FLOAT_TIM(op,pfval,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-ret.fvalue0=*pfval;\
-ret.result=0;\
+ret->fvalue0=*pfval;\
+ret->result=0;\
 accessor->send(&message);\
-*pfval = ret.fvalue0;\
-return ret.result;
+*pfval = ret->fvalue0;\
+RETURN
 
 /***************************/
 
 #define READ_OP_AX_INT_TIM(op,ax,pival,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-idata.axis=ax;\
-ret.fvalue0=*pfval;\
+idata->axis=ax;\
+ret->fvalue0=*pfval;\
 accessor->send(&message);\
-*pival = ret.ivalue;\
-return ret.result;
+*pival = ret->ivalue;\
+RETURN
 
 #define READ_OP_INT_TIM(op,pival,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-ret.ivalue=*pival;\
+ret->ivalue=*pival;\
 accessor->send(&message);\
-*pival = ret.ivalue;\
-return ret.result;
+*pival = ret->ivalue;\
+RETURN
 
 /***************************/
 #define READ_OP_AX_INT64_STRING_TIM(op,ax,pival,pstring,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-idata.axis=ax;\
-ret.alarm_mask=*pival ;\
-*ret.str=0;\
+idata->axis=ax;\
+ret->alarm_mask=*pival ;\
+*ret->str=0;\
 accessor->send(&message);\
-*pival = ret.alarm_mask;\
-pstring = ret.str;\
-return ret.result;
+*pival = ret->alarm_mask;\
+pstring = ret->str;\
+RETURN
 /***************************/
 #define READ_OP_AX_INT_STRING_TIM(op,ax,pival,pstring,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-idata.axis=ax;\
-*ret.str=0;\
-ret.ivalue=*pival;\
+idata->axis=ax;\
+*ret->str=0;\
+ret->ivalue=*pival;\
 accessor->send(&message);\
-*pival = ret.ivalue;\
-pstring = ret.str;\
-return ret.result;
-
+*pival = ret->ivalue;\
+pstring = ret->str;\
+RETURN
 #define READ_OP_INT_STRING_TIM(op,pival,pstring,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-*ret.str=0;\
-ret.ivalue=*pival;\
+*ret->str=0;\
+ret->ivalue=*pival;\
 accessor->send(&message);\
-*pival = ret.ivalue;\
-pstring = ret.str;\
-return ret.result;
-
+*pival = ret->ivalue;\
+pstring = ret->str;\
+RETURN
 /***************************/
 
 #define WRITE_OP_AX_STRING_TIM(op,ax,pstring,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-idata.axis=ax;\
-strncpy(idata.str,(char*)pstring,MAX_STR_SIZE); \
+idata->axis=ax;\
+strncpy(idata->str,(char*)pstring,MAX_STR_SIZE); \
 accessor->send(&message);\
-return ret.result;
+RETURN
 
 #define WRITE_OP_STRING_TIM(op,pstring,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-if(pstring) strncpy(idata.str,(char*)pstring,MAX_STR_SIZE); \
+if(pstring) strncpy(idata->str,(char*)pstring,MAX_STR_SIZE); \
 accessor->send(&message);\
-return ret.result;
+RETURN
 
 /***************************/
 
 
 #define WRITE_OP_STRING_STRING_TIM(op,pstring,pstring2,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-if(pstring) strncpy(idata.str,(char*)pstring,MAX_STR_SIZE); \
-if(pstring2) strncpy(idata.str2,(char*)pstring2,MAX_STR_SIZE); \
-accessor->send(&message);\
-return ret.result;
+if(pstring) strncpy(idata->str,(char*)pstring,MAX_STR_SIZE); \
+if(pstring2) strncpy(idata->str2,(char*)pstring2,MAX_STR_SIZE); \
+SEND_AND_RETURN
 
 #define WRITE_OP_AX_STRING_STRING_TIM(op,ax,pstring,pstring2,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-if(pstring) strncpy(idata.str,(char*)pstring,MAX_STR_SIZE); \
-if(pstring2) strncpy(idata.str2,(char*)pstring2,MAX_STR_SIZE); \
-idata.axis=ax;\
-accessor->send(&message);\
-return ret.result;
+if(pstring) strncpy(idata->str,(char*)pstring,MAX_STR_SIZE); \
+if(pstring2) strncpy(idata->str2,(char*)pstring2,MAX_STR_SIZE); \
+idata->axis=ax;\
+SEND_AND_RETURN
+
 /***************************/
 #define READ_OP_AX_STRING_RETSTRING_TIM(op,ax,pstring,pstring2,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-if(pstring.size()) strncpy(idata.str,pstring.c_str(),MAX_STR_SIZE); \
-idata.axis=ax;\
+if(pstring.size()) strncpy(idata->str,pstring.c_str(),MAX_STR_SIZE); \
+idata->axis=ax;\
 accessor->send(&message);\
-pstring2=ret.str; \
-return ret.result;
+pstring2=ret->str; \
+RETURN
 /***************************/
 /* 
 #define READ_OP_INT_TIM(op,pival,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
 accessor->send(&message);\
-*pival = ret.ivalue;\
-return ret.result;
+*pival = ret->ivalue;\
+return ret->result;
 */
 
 /***************************/
 #define READ_OP_64INT_TIM(op,pival,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
 accessor->send(&message);\
-*pival = ret.alarm_mask;\
-return ret.result;
+*pival = ret->alarm_mask;\
+RETURN
 
 #define READ_OP_AX_64INT_TIM(op,ax,pival,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-idata.axis=ax;\
-ret.alarm_mask=0;\
+idata->axis=ax;\
+ret->alarm_mask=0;\
 accessor->send(&message);\
-*pival = ret.alarm_mask;\
-return ret.result;
+*pival = ret->alarm_mask;\
+RETURN
 /***************************/
 
 
 #define READ_OP_64INT_TIM_NORET(op,pival,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
 accessor->send(&message);\
-*pival = ret.alarm_mask;
+*pival = ret->alarm_mask;
 
 #define READ_OP_AX_64INT_TIM_NORET(op,ax,pival,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-idata.axis=ax;\
+idata->axis=ax;\
 accessor->send(&message);\
-*pival = ret.alarm_mask;
+*pival = ret->alarm_mask;\
+RETURN
 /***************************/
 
 #define READ_OP_2FLOAT_TIM(op,pfval0,pfval1,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
 accessor->send(&message);\
-*pfval0 = ret.fvalue0;\
-*pfval1 = ret.fvalue1;\
-return ret.result;
+*pfval0 = ret->fvalue0;\
+*pfval1 = ret->fvalue1;\
+RETURN
 
 #define READ_OP_AX_2FLOAT_TIM(op,ax,pfval0,pfval1,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-idata.axis=ax;\
+idata->axis=ax;\
 accessor->send(&message);\
-*pfval0 = ret.fvalue0;\
-*pfval1 = ret.fvalue1;\
-return ret.result;
+*pfval0 = ret->fvalue0;\
+*pfval1 = ret->fvalue1;\
+RETURN
 /***************************/
 
 #define WRITE_OP_TIM(op,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-accessor->send(&message);\
-return ret.result;
-
+SEND_AND_RETURN
 
 #define WRITE_OP_AX_TIM(op,ax,timeout) \
 PREPARE_OP_RET_INT_TIMEOUT(op,timeout); \
-idata.axis=ax;\
-accessor->send(&message);\
-return ret.result;
+idata->axis=ax;\
+SEND_AND_RETURN
+
 /***************************/
 #define ACT_STD_TIMEOUT 4999
 uint64_t GeneralTimeout = ACT_STD_TIMEOUT;
